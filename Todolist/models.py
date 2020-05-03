aa64aebb62138277df754cdf98c534db05b80a73
isharrma <54413011+isharrma@users.noreply.github.com>
2020-05-03 19:13:43 +0530

    Todo_list Model
    Made the first view and the model Tod_list for the record keeping

--- Todolist/models.py
+++ Todolist/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Todo_list(models.Model):
+    pub_date = models.DateTimeField('date published')
+    todo_text =models.CharField(max_length=100)
+
+
