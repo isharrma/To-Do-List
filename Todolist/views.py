116e1caa2651820ee08eab3a1414b6779a467f00
isharrma <54413011+isharrma@users.noreply.github.com>
2020-05-05 22:26:51 +0530

    Todo function
    Added the bootstrap which is working now.
    Made a function add_todo that is redirecting but nor printing yet.

--- Todolist/views.py
+++ Todolist/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'Todolist/index.html')
+
+
+def add_todo(request):
+    return render(request, 'Todolist/index.html')
