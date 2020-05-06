12cb276206e698fd8c41dbd18da037e097effe4a
isharrma <54413011+isharrma@users.noreply.github.com>
2020-05-06 22:37:30 +0530

    Completed
    Basics Completed.
    Future Scope: Add radio buttons and some other functionalities

--- Todolist/views.py
+++ Todolist/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Todo_list
 
 
 def index(request):
-    return render(request, 'Todolist/index.html')
+    todo_items = Todo_list.objects.all().order_by('pub_date')
+    return render(request, 'Todolist/index.html',
+                    {
+                        "todo_items": todo_items
+                    })
 
-
+@csrf_exempt
 def add_todo(request):
-    return render(request, 'Todolist/index.html')
+    content = request.POST['content']
+    date = timezone.now()
+    new_obj = Todo_list.objects.create(pub_date=date, todo_text=content) 
+    print(new_obj)
+    return HttpResponseRedirect("/")
+
+@csrf_exempt
+def delete_todo(request, todo_id):
+    Todo_list.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect("/")
